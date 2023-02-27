1240d1c1e66bb2f18be970932eaf97990617cc3e
Gerard176 <duartegerardo175@gmail.com>
2023-02-26 19:19:57 -0500

    Primeros dos

---
+++ Calculadora/src/main/java/Logica/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Logica;
+
+import igu.Pantalla;
+
+/**
+ *
+ * @author ASUS M509
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Pantalla p = new Pantalla();
+        p.setVisible(true);
+        p.setLocationRelativeTo(null);
+        
+    }
+}
---
+++ CrearArreglo/src/main/java/com/mycompany/creararreglo/CrearArreglo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.creararreglo;
+
+/**
+ *
+ * @author ASUS M509
+ */
+public class CrearArreglo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
